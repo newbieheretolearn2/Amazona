eb97d52acfd26efc841870a8aabb1d860a662c61
newbieheretolearn2 <97187778+newbieheretolearn2@users.noreply.github.com>
2023-05-02 16:19:40 +0000

    frontend

---
+++ frontend/src/data.js
@@ -0,0 +1,46 @@
+ const data = {
+
+
+    products : [
+        {
+
+            name: 'Nike shirt   ',
+            slug: 'nike-slim shirt',
+            category : 'Shirts',
+            image: '/images/p1.jpg',
+            price : 120,
+            countInstock : 10,
+            brand:'Nike',
+            rating :4.5,
+            numReviews: 10,
+            description: 'shirt.'
+        },
+        {
+
+            name: 'Puma shirt   ',
+            slug: 'Puma-slim shirt',
+            category : 'Shirts',
+            image: '/images/p2.jpg',
+            price : 120,
+            countInstock : 10,
+            brand:'Puma',
+            rating :4.5,
+            numReviews: 10,
+            description: 'shirt.'
+        },
+        {
+
+            name: 'test   ',
+            slug: 'adidas-slim shirt',
+            category : 'Shirts',
+            image: '/images/p3.jpg',
+            price : 120,
+            countInstock : 10,
+            brand:'adidas',
+            rating :4.5,
+            numReviews: 10,
+            description: 'shirt.'
+        },
+    ],
+ };
+ export default data;
---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,81 @@
+import axios from 'axios';
+import logger from 'use-reducer-logger';
+import { useEffect, useReducer} from 'react';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Product from '../components/Product';
+import { Helmet } from 'react-helmet-async';
+import LoadingBox from '../components/LoadingBox';
+import MessageBox from '../components/MessageBox';
+const reducer = (state,action ) => {
+
+    switch(action.type){
+
+        case 'FECTCH_REQUEST':
+            return {...state,loading:true};
+            case 'FETCH_SUCCESS': 
+
+                return {...state,products: action.payload ,loading:false  };
+                case 'FETCH_FAIL':
+                    return {...state, loading :false, error:action.paload};
+                    default :
+                   return state;
+            
+
+    }
+}
+
+
+
+function HomeScreen(){
+    const [{loading,error,products},dispatch] = useReducer(logger(reducer),{
+        products:[],
+        loading: true,
+        error:'',
+    });
+//const [products,setProducts] = useState([]);
+useEffect(() =>{
+const fetchdata = async ( ) =>  {
+dispatch({type: 'FETCH_REQUEST'});
+try{
+
+    const result = await axios.get('/api/products');
+    dispatch({type: 'FETCH_SUCCESS',payload: result.data});
+}catch(err){
+    dispatch({type: 'FETCH_FAIL',payload:err.message});
+}
+
+//setProducts(result.data);
+};
+fetchdata();
+
+}, []);
+
+
+    return <div><h1>
+    Featured Products
+  </h1>
+  <Helmet>
+    <title>
+        Amazona
+    </title>
+  </Helmet>
+  <div className="products">
+    {
+        loading? (<LoadingBox/> ) :
+        error? (<MessageBox variant="danger">{error}</MessageBox>) : (   
+            <Row> 
+      {products.map(product => (
+      <Col key={product.slug} sm={6} md={4} lr={3} className="mb-3">
+        <Product product={product} ></Product>
+      </Col>
+      ))}
+      </Row>
+      )}
+
+    
+  </div>
+  </div>;
+}
+
+export default HomeScreen
